56fe7bb48df9c8c930bb17751418b5057ae94aba
maatouk-j1 <maatouk.j1@gmail.com>
2026-07-16 12:43:54 +0300

    Removed navigation indexes for sections: Installation of CDP 7.3.1, upgrades, use cases and troubleshooting.

--- lib/navigation.ts
+++ lib/navigation.ts
@@ -16,7 +16,10 @@ export const navigation: NavItem[] = [
             children: [
               { title: "CDP Private Cloud Overview", href: "/cdppvc/cdppvc" },
               { title: "Demo Architecture", href: "/cdppvc/demo" },
-              { title: "Installation Prerequisites", href: "/cdppvc/prerequisites" },
+              {
+                title: "Installation Prerequisites",
+                href: "/cdppvc/prerequisites",
+              },
               { title: "Cloudera Manager Deployment", href: "/cdppvc/cm" },
               {
                 title: "Base Cluster (Data Lake)",
@@ -33,7 +36,10 @@ export const navigation: NavItem[] = [
                   { title: "Overview", href: "/cdppvc/ocp" },
                   { title: "Docker Registry in Nexus", href: "/cdppvc/nexus" },
                   { title: "Vault Deployment", href: "/cdppvc/vault" },
-                  { title: "CDP Data Services Control Plane on Openshift", href: "/cdppvc/dsocp" },
+                  {
+                    title: "CDP Data Services Control Plane on Openshift",
+                    href: "/cdppvc/dsocp",
+                  },
                 ],
               },
               {
@@ -42,10 +48,22 @@ export const navigation: NavItem[] = [
                   { title: "Overview", href: "/cdppvc/ds" },
                   { title: "Data Services Console", href: "/cdppvc/dsconsole" },
                   { title: "Cloudera Machine Learning", href: "/cdppvc/cmlds" },
-                  { title: "Cloudera Data Warehouse (ECS)", href: "/cdppvc/cdwecs" },
-                  { title: "Cloudera Data Warehouse (Openshift)", href: "/cdppvc/cdwocp" },
-                  { title: "Cloudera Data Engineering (ECS)", href: "/cdppvc/cdeecs" },
-                  { title: "Cloudera Data Engineering (Openshift)", href: "/cdppvc/cdeocp" },
+                  {
+                    title: "Cloudera Data Warehouse (ECS)",
+                    href: "/cdppvc/cdwecs",
+                  },
+                  {
+                    title: "Cloudera Data Warehouse (Openshift)",
+                    href: "/cdppvc/cdwocp",
+                  },
+                  {
+                    title: "Cloudera Data Engineering (ECS)",
+                    href: "/cdppvc/cdeecs",
+                  },
+                  {
+                    title: "Cloudera Data Engineering (Openshift)",
+                    href: "/cdppvc/cdeocp",
+                  },
                 ],
               },
               {
@@ -62,16 +80,28 @@ export const navigation: NavItem[] = [
                 title: "CDP PvC Openshift: Day-2",
                 children: [
                   { title: "Overview", href: "/cdppvc/ocpday2" },
-                  { title: "Deploy CDW Disk in Openshift", href: "/cdppvc/ocpcdwdisk" },
-                  { title: "Deploy Nvidia GPU in Openshift", href: "/cdppvc/gpuocp" },
+                  {
+                    title: "Deploy CDW Disk in Openshift",
+                    href: "/cdppvc/ocpcdwdisk",
+                  },
+                  {
+                    title: "Deploy Nvidia GPU in Openshift",
+                    href: "/cdppvc/gpuocp",
+                  },
                 ],
               },
-              { title: "Expand Longhorn Disk Space", href: "/cdppvc/longhornscale" },
+              {
+                title: "Expand Longhorn Disk Space",
+                href: "/cdppvc/longhornscale",
+              },
               {
                 title: "Data Warehousing",
                 children: [
                   { title: "Overview", href: "/cdw/cdw" },
-                  { title: "Parquet, ORC, Avro and CSV Benchmarking", href: "/cdw/benchmarkfs" },
+                  {
+                    title: "Parquet, ORC, Avro and CSV Benchmarking",
+                    href: "/cdw/benchmarkfs",
+                  },
                   { title: "SNAPPY Compression", href: "/cdw/snappy" },
                   { title: "Auto-scaling in CDW", href: "/cdw/cdwautoscaling" },
                 ],
@@ -80,7 +110,10 @@ export const navigation: NavItem[] = [
                 title: "Longhorn",
                 children: [
                   { title: "Overview", href: "/longhorn/longhorn" },
-                  { title: "Performance Benchmarking", href: "/longhorn/benchmarking" },
+                  {
+                    title: "Performance Benchmarking",
+                    href: "/longhorn/benchmarking",
+                  },
                 ],
               },
               {
@@ -101,23 +134,7 @@ export const navigation: NavItem[] = [
               },
             ],
           },
-          {
-            title: "CDP 7.3.1",
-            href: "/installations/cdp-7.3.1",
-          },
         ],
-      },
-      {
-        title: "Upgrades",
-        href: "/upgrades/7.1.9-to-7.3.1/index",
-      },
-      {
-        title: "Use Cases",
-        href: "/use-cases/medallion-architecture/index",
-      },
-      {
-        title: "Troubleshooting",
-        href: "/troubleshooting/index",
       },
     ],
   },
